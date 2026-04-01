b5a024b4c47b6fd44410290fdc04643699ffb1d3
dounabou321-ux <dounabou321@gmail.com>
2026-04-01 19:43:00 +0200

    `Added registration of CustomerPayment and SupplierPayment models in payments/admin.py and SaleItem model in sales/admin.py`

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 
+from backend.payments.models import CustomerPayment,SupplierPayment
+
 # Register your models here.
+admin.site.register(SupplierPayment)
+admin.site.register(CustomerPayment)
 
